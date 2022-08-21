070f61344041449b755a0df03aeebb28da9f72d7
Ryohei-Funamoto <r.funamoto242@gmail.com>
2022-08-22 06:41:27 +0900

    .testのJSの記述を削除

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,13 +1,5 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const test = document.querySelector('.js-test');
-
-  test.addEventListener('mouseover', function () {
-    test.style.backgroundColor = 'blue';
-  });
-
-  test.addEventListener('mouseleave', function () {
-    test.style.backgroundColor = 'red';
-  });
+  
 });
