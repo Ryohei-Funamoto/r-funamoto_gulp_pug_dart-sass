2b9ee41ad0e6320731c3fcc863a4377925d5c4a4
Ryohei-Funamoto <r.funamoto242@gmail.com>
2022-08-22 07:03:14 +0900

    トップへ戻るボタンを追加

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  
+  /**
+   * トップへ戻るボタン
+   */
+  const toTop = document.querySelector('.js-to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+      toTop.classList.add('is-show');
+    } else {
+      toTop.classList.remove('is-show');
+    }
+  });
+
+  toTop.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
 });
