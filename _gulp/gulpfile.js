9f702123e39b5f4a60c15969caf6031c9ef1f9ad
Ryohei-Funamoto <r.funamoto242@gmail.com>
2022-08-24 14:34:48 +0900

    delPathを元に戻す

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -74,12 +74,12 @@ else if (env === 'development') {
 const del = require('del');
 
 const delPath = {
-  'css': distBase + '/css/**/*.css',
-  'js': distBase + '/js/**/*.js',
+  'css': distBase + '/css/**',
+  'js': distBase + '/js/**',
   'img': distBase + '/img/**',
   'html': distBase + '/**/*.html',
-  // 'wpCss': serverBase + '/css/**/*.css',
-  // 'wpJs': serverBase + '/js/**/*.js',
+  // 'wpCss': serverBase + '/css/**',
+  // 'wpJs': serverBase + '/js/**',
   // 'wpImg': serverBase + '/img/**'
 };
 const clean = (done) => {
